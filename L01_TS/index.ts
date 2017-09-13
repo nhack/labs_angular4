02a0a4cdeea69e5d7c9125b8a50ce8918cf5a386
Marius Cristea <marius.cristea@irian.ro>
2017-09-13 17:45:46 +0300

    L01.02 - TS OOP

--- L01_TS/index.ts
+++ L01_TS/index.ts
@@ -1,1 +1,36 @@
-console.log('Hello TS');
+class Person {
+    constructor(protected first: string,
+        protected last: string,
+        protected age: number) {
+    }
+
+    public isYoung = () => this.age < 30;
+
+    public toString = () => 'Person: ' + this.first + ' ' + this.last + '[' + this.age + ']';
+
+    public delayedLog = () => setTimeout(() => console.log(this.toString()), 1000);
+}
+
+class User extends Person {
+    constructor(first: string,
+        last: string,
+        age: number,
+        private password: string) {
+        super(first, last, age);
+    }
+
+    private encryptPassword = () => this.password.replace(/./g, '*');
+
+    public toString = () => 'User: ' + this.first + ' '
+        + this.last + '[' + this.age + ']' + ': '
+        + (this.password ? this.encryptPassword() : 'null');
+}
+
+let person = new Person('suveti', 'george', 24);
+console.log(person);
+console.log(person.toString());
+person.delayedLog();
+
+let user = new User('marius', 'cristea', 30, 'secret');
+console.log(user);
+console.log(user.toString());
